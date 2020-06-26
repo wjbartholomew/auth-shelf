bd8c43d772c2d3502dba6299fed6b700bd02ed6b
Samuel Villalva Lijo <samuelvillalvalijo@gmail.com>
2020-06-26 10:08:42 -0500

    Post Saga Ready for testing

--- src/redux/sagas/infoSaga.js
+++ src/redux/sagas/infoSaga.js
@@ -2,16 +2,21 @@ import { put, takeLatest } from 'redux-saga/effects';
 import axios from 'axios';
 
 //write POST saga to handle input from the INFO page
-function* inforSaga(action){
+function* postToShelf(action){
+    
     try{
         //create POST request to server
-        yield axios.post('/api/shelf', action.payload);
+        yield axios.post('/api/shelf', action.payload, config);
 
         //write dispatch to the reducer?
     }
     catch(error){
         console.log('Error with shelf post:', error);
     }
+}
+
+function* infoSaga(){
+    yield takeLatest('ADD_TO_SHELF',postToShelf);
 }
 
 
